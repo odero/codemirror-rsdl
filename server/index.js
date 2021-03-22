f3a5b80b2f7081b950de2c41eac9157c49e44f1b
Billy Odero <biodero@microsoft.com>
2021-03-22 10:55:18 +0300

    Minor cleanup

--- server/index.js
+++ server/index.js
@@ -11,10 +11,9 @@ app.use(express.json());
 app.post('/', async (req, res) => {
     const filePath = "data.rsdl";
     const pathToRapid = "C:\\Users\\git\\odata-rapid\\tools\\rsdl\\rapid-cli\\rapid\\rapid.csproj";
-    let rsdl = JSON.stringify(req.body.data.state);
+    let rsdl = JSON.stringify(req.body.data.state).replace(/\\n/g, "\r\n").replace(/"/g, "");
     let command = `dotnet run --project ${pathToRapid} ${filePath}`;
-    rsdl = rsdl.replace(/\\n/g, "\r\n").replace(/"/g, "");
-    console.log("body", rsdl);
+
     fs.writeFile(filePath, rsdl, (err, data) => {
         if (err) return console.error(err);
         
